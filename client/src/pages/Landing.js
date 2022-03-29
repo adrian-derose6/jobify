ca5203126dc588714bf9c68516fab1d02dc7bfd1
Adrian DeRose <adrian.derose6@gmail.com>
2022-03-29 14:47:38 -0500

    Setup Landing Page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,13 +1,29 @@
 import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
 
 const Landing = () => {
 	return (
-		<main>
+		<Wrapper>
 			<nav>
 				<img src={logo} alt='jobify' className='logo' />
 			</nav>
-		</main>
+			<div className='container page'>
+				{/* Info */}
+				<div className='info'>
+					<h1>
+						Job <span>tracking</span> app
+					</h1>
+					<p>
+						Track your job applications and keep your job search organized, all
+						in one place
+					</p>
+					<button className='btn btn-hero'>Login/Register</button>
+				</div>
+				<img src={main} alt='job-hunt' className='img main-img' />
+			</div>
+		</Wrapper>
 	);
 };
+
 export default Landing;
