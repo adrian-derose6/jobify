d109890520beabd9a37d85a8f1078b7db4a56bf5
Adrian DeRose <adrian.derose6@gmail.com>
2022-03-29 14:12:47 -0500

    Initialize Project

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,13 @@
+import logo from '../assets/images/logo.svg';
+import main from '../assets/images/main.svg';
+
+const Landing = () => {
+	return (
+		<main>
+			<nav>
+				<img src={logo} alt='jobify' className='logo' />
+			</nav>
+		</main>
+	);
+};
+export default Landing;
