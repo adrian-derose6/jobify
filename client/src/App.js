cd1d6c1557f7ad9d29015bf28e564135e38b5c6b
Adrian DeRose <adrian.derose6@gmail.com>
2022-03-29 15:21:17 -0500

    Add Error page to router

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,15 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Landing from './pages/Landing';
-import styled from 'styled-components';
+import { Dashboard, Landing, Register, Error } from './pages';
 
 export default function App() {
 	return (
 		<BrowserRouter>
-			<nav>
-				<Link to='/'>Dashboard</Link>
-				<Link to='/register'>Register</Link>
-				<Link to='/landing'>Dashboard</Link>
-			</nav>
 			<Routes>
-				<Route path='/' element={<div>Dashboard</div>} />
-				<Route path='/register' element={<div>Register</div>} />
+				<Route path='/' element={<Dashboard />} />
+				<Route path='/register' element={<Register />} />
 				<Route path='/landing' element={<Landing />} />
-				<Route path='*' element={<div>Error</div>} />
+				<Route path='*' element={<Error />} />
 			</Routes>
 		</BrowserRouter>
 	);
