312a6184be0349aa1e3f228157187116445cc1be
Adrian DeRose <adrian.derose6@gmail.com>
2022-03-29 15:08:16 -0500

    Setup React Router in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
 import Landing from './pages/Landing';
 import styled from 'styled-components';
 
 export default function App() {
 	return (
-		<div>
-			<Landing />
-		</div>
+		<BrowserRouter>
+			<nav>
+				<Link to='/'>Dashboard</Link>
+				<Link to='/register'>Register</Link>
+				<Link to='/landing'>Dashboard</Link>
+			</nav>
+			<Routes>
+				<Route path='/' element={<div>Dashboard</div>} />
+				<Route path='/register' element={<div>Register</div>} />
+				<Route path='/landing' element={<Landing />} />
+				<Route path='*' element={<div>Error</div>} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,12 +1,14 @@
-import logo from '../assets/images/logo.svg';
-import main from '../assets/images/main.svg';
+import { Link } from 'react-router-dom';
+
+import { Logo } from '../components';
 import Wrapper from '../assets/wrappers/LandingPage';
+import main from '../assets/images/main-alternative.svg';
 
 const Landing = () => {
 	return (
 		<Wrapper>
 			<nav>
-				<img src={logo} alt='jobify' className='logo' />
+				<Logo />
 			</nav>
 			<div className='container page'>
 				{/* Info */}
@@ -18,7 +20,9 @@ const Landing = () => {
 						Track your job applications and keep your job search organized, all
 						in one place
 					</p>
-					<button className='btn btn-hero'>Login/Register</button>
+					<Link to='/register' className='btn btn-hero'>
+						Login/Register
+					</Link>
 				</div>
 				<img src={main} alt='job-hunt' className='img main-img' />
 			</div>
