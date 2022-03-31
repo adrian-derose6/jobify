2ba6143119065c2b8b588c62931a1567d0cb4c2b
Adrian DeRose <adrian.derose6@gmail.com>
2022-03-31 14:29:50 -0500

    Setup Bad Request error and Not Found errors classes in errors folder

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,13 @@
 import { StatusCodes } from 'http-status-codes';
-import User from '../models/User.js';
 
-class CustomAPIError extends Error {
-	constructor(message) {
-		super(message);
-		this.statusCode = StatusCodes.BAD_REQUEST;
-	}
-}
+import User from '../models/User.js';
+import { BadRequestError } from '../errors/index.js';
 
 export const register = async (req, res) => {
 	const { name, email, password } = req.body;
 
 	if (!name || !email || !password) {
-		throw new CustomAPIError('Please provide all values');
+		throw new BadRequestError('Please provide all values');
 	}
 	const user = await User.create({ name, email, password });
 	res.status(StatusCodes.CREATED).json({ user });
