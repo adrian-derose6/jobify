b40f757945241ceabcc2127c448d5f1eaa04d535
Adrian DeRose <adrian.derose6@gmail.com>
2022-03-31 14:00:44 -0500

    Check for empty values in Register controller

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -5,12 +5,11 @@ const errorHandlerMiddleware = (err, req, res, next) => {
 
 	const defaultError = {
 		statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-		msg: 'Something went wrong, try again later',
+		msg: err.message || 'Something went wrong, try again later',
 	};
 
 	if (err.name === 'ValidationError') {
 		defaultError.statusCodes = StatusCodes.BAD_REQUEST;
-		//defaultError.msg = err.message;
 		defaultError.msg = Object.values(err.errors)
 			.map((item) => item.message)
 			.join(', ');
@@ -21,7 +20,6 @@ const errorHandlerMiddleware = (err, req, res, next) => {
 		defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`;
 	}
 
-	//res.status(defaultError.statusCode).json({ msg: err });
 	res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
