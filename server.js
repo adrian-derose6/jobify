9080ed39e9daba8b8b3413ff14c942152ef694f5
Adrian DeRose <adrian.derose6@gmail.com>
2022-03-30 23:06:34 -0500

    Connect sever to MongoDB Atlas database

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+import connectDB from './db/connect-db';
+
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
+
+dotenv.config();
 
 const app = express();
 
@@ -13,6 +19,15 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-	console.log(`Server is listening on port ${port}`);
-});
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URL);
+		app.listen(port, () => {
+			console.log(`Server is listening on port ${port}`);
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
