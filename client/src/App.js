c976aaaae37adeacda63b531d67a0a23f1d4b782
Adrian DeRose <adrian.derose6@gmail.com>
2022-04-07 12:32:38 -0500

    Nest dashboard subroutes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { Dashboard, Landing, Register, Error } from './pages';
+import { Landing, Register, Error } from './pages';
+import {
+	AddJob,
+	AllJobs,
+	Profile,
+	Stats,
+	SharedLayout,
+} from './pages/dashboard';
 
 export default function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path='/' element={<Dashboard />} />
+				<Route index path='/' element={<SharedLayout />}>
+					<Route path='stats' element={<Stats />} />
+					<Route path='all-jobs' element={<AllJobs />} />
+					<Route path='add-job' element={<AddJob />} />
+					<Route path='profile' element={<Profile />} />
+				</Route>
 				<Route path='/register' element={<Register />} />
 				<Route path='/landing' element={<Landing />} />
 				<Route path='*' element={<Error />} />
