7acb73b918496b7a857a11dee7ee9a8927231a72
Adrian DeRose <adrian.derose6@gmail.com>
2022-04-12 12:38:48 -0500

    Add big screen sidebar

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -2,6 +2,7 @@ import { NavLink } from 'react-router-dom';
 import { FaTimes } from 'react-icons/fa';
 
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import NavLinks from './NavLinks';
 import Logo from './Logo';
 import { useAppContext } from '../context/appContext';
 import links from '../utils/links';
@@ -23,25 +24,7 @@ const SmallSidebar = () => {
 					<header>
 						<Logo />
 					</header>
-					<div className='nav-links'>
-						{links.map((link) => {
-							const { text, path, id, icon } = link;
-
-							return (
-								<NavLink
-									to={path}
-									className={({ isActive }) =>
-										isActive ? 'nav-link active' : 'nav-link'
-									}
-									ket={id}
-									onClick={toggleSidebar}
-								>
-									<span className='icon'>{icon}</span>
-									{text}
-								</NavLink>
-							);
-						})}
-					</div>
+					<NavLinks onClick={toggleSidebar} />
 				</div>
 			</div>
 		</Wrapper>
