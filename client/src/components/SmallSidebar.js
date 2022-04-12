7b210763af4c06d45520b185bbdd06980e0bd7d6
Adrian DeRose <adrian.derose6@gmail.com>
2022-04-12 12:25:28 -0500

    Add Small screen sidebar

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,5 +1,50 @@
+import { NavLink } from 'react-router-dom';
+import { FaTimes } from 'react-icons/fa';
+
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import Logo from './Logo';
+import { useAppContext } from '../context/appContext';
+import links from '../utils/links';
+
 const SmallSidebar = () => {
-	return <Wrapper>SmallSidebar</Wrapper>;
+	const { showSidebar, toggleSidebar } = useAppContext();
+
+	return (
+		<Wrapper>
+			<div
+				className={
+					showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+				}
+			>
+				<div className='content'>
+					<button className='close-btn' onClick={toggleSidebar}>
+						<FaTimes />
+					</button>
+					<header>
+						<Logo />
+					</header>
+					<div className='nav-links'>
+						{links.map((link) => {
+							const { text, path, id, icon } = link;
+
+							return (
+								<NavLink
+									to={path}
+									className={({ isActive }) =>
+										isActive ? 'nav-link active' : 'nav-link'
+									}
+									ket={id}
+									onClick={toggleSidebar}
+								>
+									<span className='icon'>{icon}</span>
+									{text}
+								</NavLink>
+							);
+						})}
+					</div>
+				</div>
+			</div>
+		</Wrapper>
+	);
 };
 export default SmallSidebar;
