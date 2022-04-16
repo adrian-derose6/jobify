cc033c8bef163e7790d58df068de0359dde557fe
Adrian DeRose <adrian.derose6@gmail.com>
2022-04-16 17:55:40 -0500

    Create deleteJob api handler in client

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -21,6 +21,7 @@ import {
 	GET_JOBS_BEGIN,
 	GET_JOBS_SUCCESS,
 	SET_EDIT_JOB,
+	DELETE_JOB_BEGIN,
 } from './actions';
 
 const token = localStorage.getItem('token');
@@ -250,8 +251,15 @@ const AppProvider = ({ children }) => {
 		console.log('edit job');
 	};
 
-	const deleteJob = (id) => {
-		console.log(`Delete: ${id}`);
+	const deleteJob = async (jobId) => {
+		dispatch({ type: DELETE_JOB_BEGIN });
+
+		try {
+			await authFetch.delete(`/jobs/${jobId}`);
+			getJobs();
+		} catch (error) {
+			logoutUser();
+		}
 	};
 
 	return (
