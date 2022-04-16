9bbeec71776e585b627b0eaa4d6ea798ae18e317
Adrian DeRose <adrian.derose6@gmail.com>
2022-04-16 18:39:48 -0500

    Create editJob api handler in App Provider

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -22,6 +22,9 @@ import {
 	GET_JOBS_SUCCESS,
 	SET_EDIT_JOB,
 	DELETE_JOB_BEGIN,
+	EDIT_JOB_BEGIN,
+	EDIT_JOB_SUCCESS,
+	EDIT_JOB_ERROR,
 } from './actions';
 
 const token = localStorage.getItem('token');
@@ -243,12 +246,34 @@ const AppProvider = ({ children }) => {
 		clearAlert();
 	};
 
-	const setEditJob = (id) => {
-		dispatch({ type: SET_EDIT_JOB, payload: { id } });
+	const setEditJob = (jobId) => {
+		dispatch({ type: SET_EDIT_JOB, payload: { jobId } });
 	};
 
-	const editJob = () => {
-		console.log('edit job');
+	const editJob = async () => {
+		dispatch({ type: EDIT_JOB_BEGIN });
+
+		try {
+			const { position, company, jobLocation, jobType, status, editJobId } =
+				state;
+
+			await authFetch.patch(`/jobs/${editJobId}`, {
+				company,
+				position,
+				jobLocation,
+				jobType,
+				status,
+			});
+			dispatch({ type: EDIT_JOB_SUCCESS });
+			dispatch({ type: CLEAR_VALUES });
+		} catch (error) {
+			if (error.response.status === 401) return;
+			dispatch({
+				type: EDIT_JOB_ERROR,
+				payload: { msg: error.response.data.msg },
+			});
+		}
+		clearAlert();
 	};
 
 	const deleteJob = async (jobId) => {
