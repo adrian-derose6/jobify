acb2bce7a6dc243bfef9f87fa55d99d8907c85d5
Adrian DeRose <adrian.derose6@gmail.com>
2022-04-18 13:53:37 -0500

    Setup Monthly Applications charts with Rechart

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,4 +1,27 @@
+import { useState } from 'react';
+
+import JobsBarChart from './JobsBarChart';
+import JobsAreaChart from './JobsAreaChart';
+import { useAppContext } from '../context/appContext';
+import Wrapper from '../assets/wrappers/ChartsContainer';
+
 const ChartsContainer = () => {
-	return <div>ChartsContainer</div>;
+	const [barChart, setBarChart] = useState(true);
+	const { monthlyApplications: data } = useAppContext();
+
+	const toggleChart = () => {
+		setBarChart((prevState) => !prevState);
+	};
+
+	return (
+		<Wrapper>
+			<h4>Monthly Applications</h4>
+			<button type='button' onClick={toggleChart}>
+				{barChart ? 'Area Chart' : 'Bar Chart'}
+			</button>
+			{barChart ? <JobsBarChart data={data} /> : <JobsAreaChart data={data} />}
+		</Wrapper>
+	);
 };
+
 export default ChartsContainer;
