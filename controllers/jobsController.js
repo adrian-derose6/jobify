c230b63ae7f59d56d7061eccde55c3b179280a13
Adrian DeRose <adrian.derose6@gmail.com>
2022-04-18 11:05:23 -0500

    Use MongoDb aggregation pipeline & moment.js to send monthly applications stats

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,5 +1,6 @@
 import { StatusCodes } from 'http-status-codes';
 import mongoose from 'mongoose';
+import moment from 'moment';
 
 import Job from '../models/Job.js';
 import {
@@ -86,7 +87,40 @@ export const showStats = async (req, res) => {
 		interview: stats.interview || 0,
 		declined: stats.declines || 0,
 	};
-	let monthlyApplications = [];
+
+	let monthlyApplications = await Job.aggregate([
+		{ $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+		{
+			$group: {
+				_id: {
+					year: {
+						$year: '$createdAt',
+					},
+					month: {
+						$month: '$createdAt',
+					},
+				},
+				count: { $sum: 1 },
+			},
+		},
+		{ $sort: { '_id.year': -1, '_id.month': -1 } },
+		{ $limit: 6 },
+	]);
+
+	monthlyApplications = monthlyApplications
+		.map((item) => {
+			const {
+				_id: { year, month },
+				count,
+			} = item;
+			const date = moment()
+				.month(month - 1)
+				.year(year)
+				.format('MMM Y');
+
+			return { date, count };
+		})
+		.reverse();
 
 	res.status(StatusCodes.OK).json({ stats: defaultStats, monthlyApplications });
 };
